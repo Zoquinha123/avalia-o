e68b0810613cb6d64e19c2019658ed3bd1f84f95
Zoquinha123 <gilson.morais@fatec.sp.gov.br>
2022-04-04 17:59:17 +0000

    gravar

--- Comoodites.java
+++ Comoodites.java
@@ -1,5 +1,5 @@
 public class Comoodites {
-    public static void main(String) args){
+    public static void main(String[] args){
         String produto[] = new String [5];
         produto[0]="Soja saca de 60kg";
         produto[1]="Boi Gordo arroba 25kg";
@@ -8,11 +8,11 @@ public class Comoodites {
         produto[4]="Milho saca 60kg";
 
         double preco []= new double [5];
-        preco[0]= 1236,00;
-        preco[1]= 360,00;
-        preco[2]= 380,00;
-        preco[3]= 1236,00;
-        preco[4]= 110,00;
+        preco[0]= 1236.00;
+        preco[1]= 360.00;
+        preco[2]= 380.00;
+        preco[3]= 1236.00;
+        preco[4]= 110.00;
 
         for(int i = 0 ;i < produto.length ; i++){
             System.out.println("O produto" + produto[i] + "tem " + preco[i] + "unidades em preco");
--- Multiplos.java
+++ Multiplos.java
@@ -1,9 +1,9 @@
 public class Multiplos {
 
     public static void main (String[] args) {
-        for(int i=1 ; i<-100 ; i++){
+        for(int i=1 ; i<= 100 ; i++){
             if(i % 6 ==0){
-                System.out.println(1);
+                System.out.println(i);
             }
         }
     }
